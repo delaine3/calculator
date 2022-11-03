c2bdf990ca0e38273ef1241a33a5daff79fcd018
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-11-02 23:03:44 -0400

    updated css

--- src/containers/CalculatorComponent.js
+++ src/containers/CalculatorComponent.js
@@ -8,7 +8,7 @@ class CalculatorComponent extends Component {
   render() {
     return (
       <div className="center">
-        <p className="blurb">This is a calculator created using React and Redux. Click on the buttons to opperate.</p>
+        <p className="blurb">This is a calculator created using React and Redux. Click on the buttons to opperate. <br/> This calculator is one of the challeneges on The Free Code Camp. To test it, find and select "Javascript Calculator" in the Test Suite in the top left. Click "Run Tests" to see how many test cases it passes.</p>
         <Calculator />
       </div>
     );
