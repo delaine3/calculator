d79ba6dfd2bcb6bead04bbe045668a2ad074221e
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-11-14 17:08:21 -0500

    Updated key pad

--- src/components/buttonsTops.js
+++ src/components/buttonsTops.js
@@ -1,20 +1,20 @@
 export const numberList = [
+  { id: "seven", num: 7 }, 
+  { id: "eight", num: 8 }, 
   { id: "nine", num: 9 },
-  { id: "eight", num: 8 },
-
-  { id: "seven", num: 7 },
+  { id: "four", num: 4 },
+  { id: "five", num: 5 },
 
   { id: "six", num: 6 },
 
-  { id: "five", num: 5 },
-
-  { id: "four", num: 4 },
-
-  { id: "three", num: 3 },
-
-  { id: "two", num: 2 },
 
   { id: "one", num: 1 },
+  { id: "two", num: 2 },
+  { id: "three", num: 3 },
+
+
+
+
 
   { id: "zero", num: 0 },
   { id: "decimal", num: "." },
